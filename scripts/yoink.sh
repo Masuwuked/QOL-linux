114c6e63205627c66d53e3a8dad4056bb9443c13
Masuwuked <74668157+Masuwuked@users.noreply.github.com>
2024-09-06 13:36:12 +0530

    Update yoink.sh

--- scripts/yoink.sh
+++ scripts/yoink.sh
@@ -1,5 +1,12 @@
 #!/bin/zsh
 
+
+copy(){
+
+xclip -t $2 -selection clipboard < $1
+
+}
+
 if (($# == 0)); then
         echo "No file was selected. :("
         exit 1
@@ -7,15 +14,19 @@ fi
 
 local FILE=$1
 
-if [[ ! -f "$FILE" ]] then
+if [[ ! -f "$FILE" ]]; then
         echo "error 404 file NOT FOUND!"
         exit 1
 fi
 
 if [[ $(file --mime-type -b "$FILE") =~ ^image/ ]]; then
     extension=$(file -b --mime-type $FILE)
-    xclip -t "$extension" -i "$FILE"
-    echo "image? still yoinked!"
+
+      cp $FILE /tmp/image.png
+      #xclip -t image/png -selection clipboard < /tmp/image.png
+      copy /tmp/image.png image/png
+      rm /tmp/image.png
+      echo "image yoinked"
 else
     cat $FILE | xclip -selection clipboard
     echo "Yoinked!"
